1490b829724d32b806ade7614f09d734d6de7e15
kalfeltzdx <djony.malta@gmail.com>
2023-01-25 22:03:47 -0300

    Created Post Mehtod by Json format

--- src/test/java/VideoGameTests.java
+++ src/test/java/VideoGameTests.java
@@ -6,10 +6,25 @@ import org.junit.jupiter.api.Test;
 public class VideoGameTests extends VideoGameConfig {
 
     @Test
-    public void getAllGames(){
+    public void getAllGames() {
 
         RestAssured.given().when().get(VideoGameEndPoints.ALL_VIDEO_GAMES).then();
 
+    }
 
+    @Test
+    public void createNewGameBYJSON(){
+        String gameBodyJson = "{\n" +
+                "  \"category\": \"Platform\",\n" +
+                "  \"name\": \"Mario\",\n" +
+                "  \"rating\": \"Mature\",\n" +
+                "  \"releaseDate\": \"2023-01-25\",\n" +
+                "  \"reviewScore\": 89\n" +
+                "}";
+        RestAssured.given()
+                            .body(gameBodyJson)
+                    .when()
+                            .post(VideoGameEndPoints.ALL_VIDEO_GAMES)
+                    .then();
     }
 }

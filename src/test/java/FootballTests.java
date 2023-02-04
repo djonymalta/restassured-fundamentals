e16b8aea63d1a10e5f8d160028c46fbbfb47f41a
kalfeltzdx <djony.malta@gmail.com>
2023-02-04 16:21:06 -0300

    Created Assert the body of HTTP Response

--- src/test/java/FootballTests.java
+++ src/test/java/FootballTests.java
@@ -1,5 +1,7 @@
 import Config.FootballConfig;
 import io.restassured.RestAssured;
+
+import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Test;
 
 public class FootballTests extends FootballConfig {
@@ -26,4 +28,22 @@ public class FootballTests extends FootballConfig {
 
     }
 
+    @Test
+    public void getDateFounded(){
+        RestAssured
+                .get("/teams/57")
+                .then()
+                .body("founded", CoreMatchers.equalTo(1886));
+    }
+
+    @Test
+    public void getFirstTeamName(){
+        RestAssured
+                .given()
+                .when()
+                .get("competitions/2021/teams")
+                .then()
+                .body("teams.name[0]", CoreMatchers.equalTo("Arsenal FC"));
+
+    }
 }

b92cf773ab52cadb5a734695ed0e5277073b0b21
kalfeltzdx <djony.malta@gmail.com>
2023-02-10 00:12:03 -0300

    Measuring Response Time in REST Assured

--- src/test/java/Config/VideoGameConfig.java
+++ src/test/java/Config/VideoGameConfig.java
@@ -2,8 +2,10 @@ package Config;
 
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
@@ -20,5 +22,12 @@ public class VideoGameConfig {
                 .addFilter(new RequestLoggingFilter())
                 .addFilter(new ResponseLoggingFilter())
                 .build();
+
+        RestAssured.responseSpecification = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .expectResponseTime(Matchers.lessThan(3000L))
+                .build();
+
+
     }
 }
